d51470309395ded6d46c6891aa148897c1ddb9fc
maszhaowei <maszhaowei@gmail.com>
2025-12-29 20:26:12 +0800

    refactor

--- src/tasks/AutoClaim.py
+++ src/tasks/AutoClaim.py
@@ -1,5 +1,8 @@
 from ok import TriggerTask
 
+liveEndText = "直播已结束"
+claimText = "立即领取"
+
 
 class AutoClaim(TriggerTask):
 
@@ -10,18 +13,22 @@ class AutoClaim(TriggerTask):
 
     def run(self):
         self.log_info("finding...")
-        liveEnd = self.ocr(match="直播已结束")
-        if len(liveEnd) > 0:
-            self.log_info("直播已结束", True)
-        feature = self.find_one(feature_name="coupon", vertical_variance=5)
-        if feature:
-            self.click_box(feature)
+        boxByFeature = self.find_one(feature_name="coupon", vertical_variance=5)
+        if boxByFeature:
+            self.click_box(boxByFeature)
             self.click_count += 1
             self.log_info(f"Target button found by feature({self.click_count})", True)
         else:
-            ocr_boxes = self.ocr(match="立即领取")
-            if len(ocr_boxes) > 0:
-                self.click_box(ocr_boxes[0])
-                self.click_count += 1
-                self.log_info(f"Target button found by ocr({self.click_count})", True)
+            ocrBoxes = self.ocr(match=[liveEndText, claimText])
+            for ocrBox in ocrBoxes:
+                if ocrBox.name == liveEndText:
+                    self.log_info(liveEndText, True)
+                    break
+                elif ocrBox.name == claimText:
+                    self.click_box(ocrBox)
+                    self.click_count += 1
+                    self.log_info(
+                        f"Target button found by ocr({self.click_count})", True
+                    )
+                    break
         self.sleep(10)
