abcb6c07b613e482fa4fdffec24797a9cc1182d0
maszhaowei <maszhaowei@gmail.com>
2026-01-12 19:12:57 +0800

    add lottery button

--- src/tasks/AutoClaim.py
+++ src/tasks/AutoClaim.py
@@ -1,7 +1,9 @@
 from ok import TriggerTask
+import re
 
 liveEndText = "直播已结束"
 claimText = "立即领取"
+lotteryTexts = ["立即参与", re.compile(r"^参与成功")]
 
 intervalKey = "检测间隔(秒)"
 defaultInterval = 30
@@ -13,25 +15,27 @@ class AutoClaim(TriggerTask):
         super().__init__(*args, **kwargs)
         self.name = "自动领取"
         self.default_config.update({intervalKey: defaultInterval})
-        self.found_count = 0
 
     def run(self):
         self.log_info(self.hwnd_title)
         boxByFeature = self.find_one(feature_name="coupon", vertical_variance=5)
         if boxByFeature:
-            self.found_count += 1
-            self.log_info(f"Target button found by feature({self.found_count})", True)
+            self.log_info(f"Target button found by feature", True)
             self.click_box(boxByFeature)
         else:
-            ocrBoxes = self.ocr(match=[liveEndText, claimText])
+            ocrBoxes = self.ocr(
+                match=[
+                    liveEndText,
+                    claimText,
+                ]
+                + lotteryTexts
+            )
             for ocrBox in ocrBoxes:
+                self.log_info(ocrBox)
                 if ocrBox.name == liveEndText:
                     self.log_info(liveEndText, True)
-                elif ocrBox.name == claimText:
-                    self.found_count += 1
-                    self.log_info(
-                        f"Target button found by ocr({self.found_count})", True
-                    )
+                else:
+                    self.log_info(f"Target button found by ocr", True)
                     self.click_box(ocrBox)
         interval = self.config.get(intervalKey)
         self.sleep(max(interval, 1) if type(interval) is int else defaultInterval)
