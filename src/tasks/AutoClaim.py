07491234d19454592512ffcf0b2a4a0dfa960b30
maszhaowei <maszhaowei@gmail.com>
2026-01-14 22:03:05 +0800

    refactor; add lottery result feature

--- src/tasks/AutoClaim.py
+++ src/tasks/AutoClaim.py
@@ -3,7 +3,8 @@ import re
 
 liveEndText = "直播已结束"
 claimText = "立即领取"
-lotteryTexts = ["立即参与", re.compile(r"^参与成功")]
+lotteryJoinTexts = ["立即参与", re.compile(r"^参与成功")]
+lotteryResult = "查看中奖名单"
 
 intervalKey = "检测间隔(秒)"
 defaultInterval = 30
@@ -18,24 +19,23 @@ class AutoClaim(TriggerTask):
 
     def run(self):
         self.log_info(self.hwnd_title)
-        boxByFeature = self.find_one(feature_name="coupon", vertical_variance=5)
-        if boxByFeature:
-            self.log_info(f"Target button found by feature", True)
-            self.click_box(boxByFeature)
-        else:
-            ocrBoxes = self.ocr(
-                match=[
-                    liveEndText,
-                    claimText,
-                ]
-                + lotteryTexts
-            )
-            for ocrBox in ocrBoxes:
-                self.log_info(ocrBox)
-                if ocrBox.name == liveEndText:
-                    self.log_info(liveEndText, True)
-                else:
-                    self.log_info(f"Target button found by ocr", True)
-                    self.click_box(ocrBox)
+        ocrBoxes = self.ocr(
+            match=[liveEndText, claimText, lotteryResult] + lotteryJoinTexts
+        )
+        for ocrBox in ocrBoxes:
+            self.log_info(ocrBox)
+            if ocrBox.name == liveEndText:
+                self.log_info(liveEndText, True)
+            elif ocrBox.name == lotteryResult:
+                closeBtn = self.find_one(
+                    feature_name="closebutton", vertical_variance=5
+                )
+                if closeBtn:
+                    self.log_info(f"Close button found by feature", True)
+                    self.click_box(closeBtn)
+            else:
+                if ocrBox.name == claimText:
+                    self.log_info(f"Claim button found by ocr", True)
+                self.click_box(ocrBox)
         interval = self.config.get(intervalKey)
         self.sleep(max(interval, 1) if type(interval) is int else defaultInterval)
